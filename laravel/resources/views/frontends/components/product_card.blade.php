1d8dc86651b4a76f66f6b9b154197f5439e69777
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2024-03-03 16:07:20 +0700

    Update product_card.blade.php

--- laravel/resources/views/frontends/components/product_card.blade.php
+++ laravel/resources/views/frontends/components/product_card.blade.php
@@ -48,7 +48,7 @@ if ($price > $ty) {
         </div>
         <div class="geodir-category-content fl-wrap">
             {{-- HuyTQB: temp --}}
-            {{-- <h3 class="title-sin_item"><a href="{{ route('product.show', ['slug' => $product->slug]) }}">{{$product->title}}</a></h3> --}}
+            {{-- <h3 class="title-sin_item"><a href="{{ route('product.show', ['slug' => $product->slug]) }}">{{$product->title}}</a></h3>   --}}
             <div class="geodir-category-content_price">{{ $formattedPrice }}</div>
             <p>{{$product->description}}</p>
             <div class="geodir-category-content-details">
