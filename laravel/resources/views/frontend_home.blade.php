1ac31c3136c1a28ff9955da857397cad254b243d
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2024-03-03 16:50:26 +0700

    Remove a newesProducts

--- laravel/resources/views/frontend_home.blade.php
+++ laravel/resources/views/frontend_home.blade.php
@@ -4,14 +4,12 @@
 <div class="content">
     <!--  section  -->
     @include('frontends.components.home_slider',['locationsStreets' => $locationsStreets, 'locationsWards' => $locationsWards, 'categories' => $categories])
-    {{-- @include('frontends.components.home_slider',['locationsStreets' => $locationsStreets, 'locationsWards' => $locationsWards]) --}}
     <!--  section  end-->
     <!-- breadcrumbs-->
     @include('frontends.components.home_breadcrumb')
     <!-- breadcrumbs end -->
     <!-- section -->
     @include('frontends.components.home_products_grid', ['newestProducts'=> $newestProducts])
-    @include('frontends.components.home_products_grid')
     <!-- section end-->
     <!-- section -->
     @include('frontends.components.home_about_wrap')
