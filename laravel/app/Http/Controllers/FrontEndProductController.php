03e846ef9f43d5377942763641ecdecdcbc79e31
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2024-03-04 15:54:19 +0700

    Add FrontendProductController

---
+++ laravel/app/Http/Controllers/FrontEndProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontEndProductController extends Controller
+{
+    public function index()
+    {
+        // Logic để hiển thị danh sách sản phẩm trên front-end
+    }
+
+    public function show($id)
+    {
+        // Logic để hiển thị thông tin chi tiết sản phẩm trên front-end dựa trên ID
+    }
+}
