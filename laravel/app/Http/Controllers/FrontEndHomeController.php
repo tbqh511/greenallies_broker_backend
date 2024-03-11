16bd05170188ad384d5f04576a1f5dca5565788c
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2024-03-11 14:23:54 +0700

    Update FrontEndHomeController.php

--- laravel/app/Http/Controllers/FrontEndHomeController.php
+++ laravel/app/Http/Controllers/FrontEndHomeController.php
@@ -50,7 +50,7 @@ class FrontEndHomeController extends Controller
             ->take($limit)
             ->get();
 
-        dd($newestProducts);
+        //dd($newestProducts);
 
         // Return the frontend_home view with the necessary data
         return view('frontend_home', [
