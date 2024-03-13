845f490928fe437535bb3dc8250ff9fbe8c37616
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2024-03-13 20:42:11 +0700

    Update FrontEndPropertiesController.php
    Initcode for properties controller

--- laravel/app/Http/Controllers/FrontEndPropertiesController.php
+++ laravel/app/Http/Controllers/FrontEndPropertiesController.php
@@ -2,9 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Property;
 use Illuminate\Http\Request;
+use App\Models\LocationsStreet;
+use App\Models\LocationsWard;
+use App\Models\ProductType;
+use App\Models\Product;
 
 class FrontEndPropertiesController extends Controller
 {
-    //
+    /**
+     * Display the detail of a product.
+     */
+    public function show(string $slug)
+    {
+        // Fetch the product based on the slug
+        $product = Property::where('slug', $slug)->first();
+
+        // Return the product detail view with the necessary data
+        return view('product', ['product' => $product]);
+    }
+
+    /**
+     * Display a listing of the products with search variables: category, ward, street, id.
+     */
+    public function index(Request $request)
+    {
+        // Get search parameters
+        $category = $request->input('category');
+        $ward = $request->input('ward');
+        $street = $request->input('street');
+        $id = $request->input('id');
+
+        // Query to fetch products based on search parameters
+        $productsQuery = Property::query();
+
+        if ($category) {
+            $productsQuery->where('category_id', $category);
+        }
+
+        if ($ward) {
+            $productsQuery->where('ward_id', $ward);
+        }
+
+        if ($street) {
+            $productsQuery->where('street_id', $street);
+        }
+
+        if ($id) {
+            $productsQuery->where('id', $id);
+        }
+
+        // Get the list of products based on the query
+        $products = $productsQuery->paginate(10);
+
+        // Return the product list view with the necessary data
+        return view('product_list', ['products' => $products]);
+    }
 }
