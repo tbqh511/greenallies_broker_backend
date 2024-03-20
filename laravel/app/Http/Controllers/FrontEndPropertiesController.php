ae3ae782e8bb7b20b2e1d3f28e2d6bb15563aff1
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2024-03-20 21:37:08 +0700

    test

--- laravel/app/Http/Controllers/FrontEndPropertiesController.php
+++ laravel/app/Http/Controllers/FrontEndPropertiesController.php
@@ -72,7 +72,7 @@ class FrontEndPropertiesController extends Controller
         // Tăng giá trị của cột total_click
         $property->increment('total_click');
 
-        dd($property->agent->count_properties);
+        dd($property->agent->customer_total_post);
 
         // Return the property detail view with the necessary data
         return view('frontend_properties_detail', [
