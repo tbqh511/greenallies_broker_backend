64f0fcb3cda90d2d002b236bdcdfdc95178a6b53
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2024-03-24 17:22:25 +0700

    Update FrontEndPropertiesController.php

--- laravel/app/Http/Controllers/FrontEndPropertiesController.php
+++ laravel/app/Http/Controllers/FrontEndPropertiesController.php
@@ -177,7 +177,7 @@ class FrontEndPropertiesController extends Controller
             // Lấy giá trị tối thiểu và tối đa của khoảng giá
             $minPrice = $priceRanges[0];
             $maxPrice = $priceRanges[1];
-            dd($maxPrice);
+            dd($maxPrice == config('global.max_price'));
             // Thêm điều kiện vào truy vấn để lấy các bất động sản trong khoảng giá
             if ($maxPrice == config('global.max_price')) {
                 // Truy vấn các bất động sản có giá lớn hơn $minPrice
