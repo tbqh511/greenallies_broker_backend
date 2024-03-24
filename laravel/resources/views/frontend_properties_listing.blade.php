30c0f1aedd3d97709be747eb9492820e15256302
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2024-03-24 20:08:33 +0700

    Update frontend_properties_listing.blade.php

--- laravel/resources/views/frontend_properties_listing.blade.php
+++ laravel/resources/views/frontend_properties_listing.blade.php
@@ -171,7 +171,7 @@
                                     <div class="listsearch-input-item">
                                         <label for="legal">Pháp lý</label>
                                         <select name="legal" data-placeholder="Chọn pháp lý" class="chosen-select on-radius no-search-select">
-                                            <option value="" {{ Request::input('legal')=='' ? 'selected' : '' }}>Chọn pháp lý</option>
+                                            <option value="" selected>Chọn pháp lý</option>
                                             @foreach ($legals as $key => $value)
                                             <option value="{{ $value }}" {{ Request::input('legal')==$key ? 'selected' : '' }}>{{ $value }}</option>
                                             @endforeach
