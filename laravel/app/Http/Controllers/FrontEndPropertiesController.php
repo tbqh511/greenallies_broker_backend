4d9011fee4dce6c2c7651bd0180b3f88e9e8a546
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2024-03-25 20:44:58 +0700

    Update FrontEndPropertiesController.php

--- laravel/app/Http/Controllers/FrontEndPropertiesController.php
+++ laravel/app/Http/Controllers/FrontEndPropertiesController.php
@@ -252,7 +252,7 @@ class FrontEndPropertiesController extends Controller
 
         // Get the list of products based on the query
         $propertiesQuery->where('status','1');
-        $properties = $propertiesQuery->paginate(6);
+        $properties = $propertiesQuery->paginate(3);
 
         //dd($areaInput, $numberFloorInput, $numberFloorInput);
         
