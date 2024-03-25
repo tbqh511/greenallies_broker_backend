f97b9361059f4ff31afe90d429a4e7c08b2d8eca
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2024-03-25 22:25:51 +0700

    Update frontend_properties_listing.blade.php

--- laravel/resources/views/frontend_properties_listing.blade.php
+++ laravel/resources/views/frontend_properties_listing.blade.php
@@ -381,7 +381,7 @@
             <!-- listing-item-wrap end-->
             <!-- pagination-->
             <div class="pagination">
-                {{-- @if ($properties->onFirstPage())
+                @if ($properties->onFirstPage())
                 <a href="#" class="prevposts-link disabled"><i class="fa fa-caret-left"></i></a>
                 @else
                 <a href="{{ $properties->previousPageUrl() }}" class="prevposts-link"><i class="fa fa-caret-left"></i></a>
@@ -399,28 +399,7 @@
                 <a href="{{ $properties->nextPageUrl() }}" class="nextposts-link"><i class="fa fa-caret-right"></i></a>
                 @else
                 <a href="#" class="nextposts-link disabled"><i class="fa fa-caret-right"></i></a>
-                @endif --}}
-
-                @if ($properties->onFirstPage())
-                    <a href="#" class="prevposts-link disabled"><i class="fa fa-caret-left"></i></a>
-                @else
-                    <a href="{{ $properties->previousPageUrl() }}" class="prevposts-link"><i class="fa fa-caret-left"></i></a>
                 @endif
-
-                @foreach ($properties->getUrlRange(1, $properties->lastPage()) as $page => $url)
-                    @if ($page == $properties->currentPage())
-                        <a href="#" class="current-page">{{ $page }}</a>
-                    @else
-                        <a href="{{ $url }}?{{ http_build_query(request()->except('page')) }}">{{ $page }}</a>
-                    @endif
-                @endforeach
-
-                @if ($properties->hasMorePages())
-                    <a href="{{ $properties->nextPageUrl() }}?{{ http_build_query(request()->except('page')) }}" class="nextposts-link"><i class="fa fa-caret-right"></i></a>
-                @else
-                    <a href="#" class="nextposts-link disabled"><i class="fa fa-caret-right"></i></a>
-                @endif
-
             </div>
             <!-- pagination end-->
         </div>
