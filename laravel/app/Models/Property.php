9000ce9c034ab832fb7e3a4c332c2dec474aaff7
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2024-03-30 13:38:10 +0700

    1

--- laravel/app/Models/Property.php
+++ laravel/app/Models/Property.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -19,7 +20,6 @@ class Property extends Model
         'street_code',
         'ward_code',
         'host_id',
-
         'title',
         'description',
         'address',
