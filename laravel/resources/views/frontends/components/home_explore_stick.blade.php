58c852b19e3070cf1f41e1577b26a8961e3598d6
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2024-03-30 13:49:18 +0700

    Update home_explore_stick.blade.php

--- laravel/resources/views/frontends/components/home_explore_stick.blade.php
+++ laravel/resources/views/frontends/components/home_explore_stick.blade.php
@@ -1,7 +1,7 @@
 <div class="slick-item">
     <div class="half-carousel-item fl-wrap">
         <div class="bg-wrap bg-parallax-wrap-gradien">
-            <div class="bg" data-bg="/images/bg/phuong1.jpg"></div>
+            <div class="bg" data-bg="/images/bg/{{$locationsWard->code}}.jpg"></div>
         </div>
         <div class="half-carousel-content">
             <a href="{{ route('properties.index', ['ward' => $locationsWard->code]) }}" class="hc-counter small-btn color-bg">{{ $locationsWard->properties_count }} BĐS đang giao dịch</a>
