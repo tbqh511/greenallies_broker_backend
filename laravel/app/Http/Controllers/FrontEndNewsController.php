819f34affddcdb9ab6e6280a719cdd25e91f2a30
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2024-03-31 14:44:54 +0700

    Update FrontEndNewsController.php

--- laravel/app/Http/Controllers/FrontEndNewsController.php
+++ laravel/app/Http/Controllers/FrontEndNewsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\LocationsWard;
 use Illuminate\Http\Request;
 
 class FrontEndNewsController extends Controller
@@ -9,36 +11,21 @@ class FrontEndNewsController extends Controller
     ///*** Display the detail of a property by its ID.*/
     public function getAgentById(int $id)
     {
-        // // Fetch the property based on the ID
-        // $property = Property::findOrFail($id);
+        // Get the district code from configuration
+        $districtCode = config('location.district_code', null);
 
-        // // Set parameters for the product query
-        // $offset = 0;
-        // $limit = 6;
-        // $sort = 'updated_at';
-        // $order = 'DESC';
+        // Get the list of wards based on the district code
+        $locationsWards = LocationsWard::when($districtCode, function ($query) use ($districtCode) {
+            return $query->where('district_code', $districtCode);
+        })->get()->sortBy('full_name');
+
+        // Get category for header section
+        $categories = Category::orderBy('category')->get();
         
-        // $newestProperties = Property::with('customer')
-        //     ->with('user')
-        //     ->with('category:id,category,image')
-        //     ->with('assignfacilities.outdoorfacilities')
-        //     ->with('favourite')
-        //     ->with('parameters')
-        //     ->with('interested_users')
-        //     ->with('ward')
-        //     ->with('street')
-        //     ->with('host')
-        //     ->orderBy($sort, $order)
-        //     ->skip($offset)
-        //     ->take($limit)
-        //     ->get();
-
-        // // Return the property detail view with the necessary data
-        // return view('frontend_properties_detail', [
-        //     'property' => $property,
-        //     'newestProperties' => $newestProperties,
-        // ]);
-        return view('frontend_news_detail');
+        return view('frontend_news_detail',[
+            'locationsWards'=> $locationsWards,
+            'categories'=> $categories
+        ]);
     }
 
     /**
@@ -46,37 +33,21 @@ class FrontEndNewsController extends Controller
      */
     public function index(Request $request)
     {
-        // // Get search parameters
-        // $category = $request->input('category');
-        // $ward = $request->input('ward');
-        // $street = $request->input('street');
-        // $id = $request->input('id');
 
-        // // Query to fetch properties based on search parameters
-        // $propertiesQuery = Property::query();
+        // Get the district code from configuration
+        $districtCode = config('location.district_code', null);
 
-        // if ($category) {
-        //     $propertiesQuery->where('category_id', $category);
-        // }
+        // Get the list of wards based on the district code
+        $locationsWards = LocationsWard::when($districtCode, function ($query) use ($districtCode) {
+            return $query->where('district_code', $districtCode);
+        })->get()->sortBy('full_name');
 
-        // if ($ward) {
-        //     $propertiesQuery->where('ward_id', $ward);
-        // }
-
-        // if ($street) {
-        //     $propertiesQuery->where('street_id', $street);
-        // }
-
-        // if ($id) {
-        //     $propertiesQuery->where('id', $id);
-        // }
-
-        // // Get the list of properties based on the query
-        // $properties = $propertiesQuery->paginate(6);
-
-        // // Return the property list view with the necessary data
-        // return view('frontend_properties_listing', ['properties' => $properties]);
-
-         return view('frontend_news_listing');
+        // Get category for header section
+        $categories = Category::orderBy('category')->get();
+        
+         return view('frontend_news_listing',[
+            'locationsWards'=> $locationsWards,
+            'categories'=> $categories
+        ]);
     }
 }
