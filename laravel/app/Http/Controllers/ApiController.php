95bf405bad97d44fbe422eb113646505d2a66bd4
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2024-06-07 15:53:46 +0700

    test

--- laravel/app/Http/Controllers/ApiController.php
+++ laravel/app/Http/Controllers/ApiController.php
@@ -1138,17 +1138,18 @@ class ApiController extends Controller
                         }
                     }
                     /// START :: HuyTBQ : Update property type
-                    if (isset($request->property_type)) {
-                        if ($request->propery_type == 'Sell') {
-                            $property->property_type = 0;
-                        } elseif ($request->propery_type == 'Rent') {
-                            $property->property_type = 1;
-                        } elseif ($request->propery_type == 'Sold') {
-                            $property->property_type = 2;
-                        } elseif ($request->propery_type == 'Rented') {
-                            $property->property_type = 3;
-                        }
-                    }
+                    $property->property_type = 1;
+                    // if (isset($request->property_type)) {
+                    //     if ($request->propery_type == 'Sell') {
+                    //         $property->property_type = 0;
+                    //     } elseif ($request->propery_type == 'Rent') {
+                    //         $property->property_type = 1;
+                    //     } elseif ($request->propery_type == 'Sold') {
+                    //         $property->property_type = 2;
+                    //     } elseif ($request->propery_type == 'Rented') {
+                    //         $property->property_type = 3;
+                    //     }
+                    // }
                     /// END :: HuyTBQ : Update property type
                     /// START :: HuyTBQ : Update host module
                     $hostId = $request->host_id;
