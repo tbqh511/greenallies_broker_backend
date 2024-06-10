5fe99e7d8c3549aa687d16eaec9b23e9d23eacb7
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2024-06-10 20:48:24 +0700

    add order column

--- laravel/database/migrations/2024_06_10_134423_add_order_to_parameters_table.php
+++ laravel/database/migrations/2024_06_10_134423_add_order_to_parameters_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('parameters', function (Blueprint $table) {
-            //
+            $table->integer('order')->default(0); // Thêm cột order
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('parameters', function (Blueprint $table) {
-            //
+            $table->dropColumn('order'); // Xóa cột order nếu rollback
         });
     }
 };
