919f69d7aa336f7607695e60c3071639a46993fd
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2024-07-21 15:25:57 +0700

    dupplicate defination fix bug

--- laravel/config/constants.php
+++ laravel/config/constants.php
@@ -1,7 +1,7 @@
 <?php
 
 
-define('PERMISSION_ERROR_MSG', 'You are not authorize to operate on the module ');
+//define('PERMISSION_ERROR_MSG', 'You are not authorize to operate on the module ');
 define('JWT_SECRET_KEY', '404D635166546A576E5A7234753778214125442A472D4B614E645267556B5870');
 
 defined('PAYPAL_SANDBOX_MODE') or define('PAYPAL_SANDBOX_MODE', true);
