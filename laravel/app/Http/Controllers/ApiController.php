fc0a8e1c730e5ccb7d5e86bda43a6cbdee710bab
tbqh511 <tbqh0511@gmail.com>
2024-11-03 15:18:48 +0700

    2

--- laravel/app/Http/Controllers/ApiController.php
+++ laravel/app/Http/Controllers/ApiController.php
@@ -2222,12 +2222,15 @@ class ApiController extends Controller
                 if (file_exists($json_file_path)) {
                     $json_string = file_get_contents($json_file_path);
                     $json_data = json_decode($json_string, true); // Thêm 'true' để trả về mảng thay vì object
-                    json_encode($json_data);
+                    
 
                     if ($json_data !== null) {
                         // Chuyển mảng thành chuỗi JSON để tránh lỗi
+                        $language->file_name1 = json_encode($json_data);;
+                        $language->json_file_path = $json_file_path;
+                        $language->json_string = $json_string;
                         $language->file_name = $json_data;
-                    
+
                         $response['error'] = false;
                         $response['message'] = "Data Fetch Successfully";
                         $response['data'] = $language;
